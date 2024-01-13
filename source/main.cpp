33fdcfed5df143d80b79103a6cdb2e1db3e5dbf2
dnarok <jeppreston@gmail.com>
2024-01-13 01:43:27 -0500

    Added my contract library as a dependency. Remove spdlog submodule due to transitive dependency from contract. Adjusted hello world to include contract.

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,13 @@
 #include "spdlog/spdlog.h"
 #include "fmt/core.h"
+#include "contract.hpp"
 
 auto main() -> int
 {
+    contract;
+        pre(1 == 2);
+        post(2 == 3);
     spdlog::info("Hello, {}!", fmt::format("{}{}", "wor", "ld"));
+
+    cond(1 == 3);
 };
